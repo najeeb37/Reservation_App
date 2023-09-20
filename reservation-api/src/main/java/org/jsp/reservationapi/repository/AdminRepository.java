b17f1016a3987dbf6044640c77949301d0cc7304
najeeb37 <najeebnaju323@gmail.com>
2023-09-20 07:44:37 +0530

    Updated Admin Repository package

--- reservation-api/src/main/java/org/jsp/reservationapi/repository/AdminRepository.java
+++ reservation-api/src/main/java/org/jsp/reservationapi/repository/AdminRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
-	@Query("select a from Admin a where a.phone =?1 and a.password=?2")
-	public Optional<Admin> verifyAdmin(long phone,String password);
+//	@Query("select a from Admin a where a.phone =?1 and a.password=?2")
+//	public Optional<Admin> verifyAdmin(long phone,String password);
 	
-//	@Query("select a from Admin a where a.email=?1 and a.password=?2")
-//	public Optional<Admin> verifyAdmin(long email,String password);
+	@Query("select a from Admin a where a.email=?1 and a.password=?2")
+	public Optional<Admin> verifyAdmin(String email,String password);
 }
